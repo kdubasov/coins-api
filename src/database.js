5ebbd17378660e55078973f35e7730fb606f8e21
integ <cergocergo41@gmail.com>
2022-10-20 17:15:07 +0300

    add database.js

---
+++ src/database.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getDatabase} from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyArfk3tVIuyrqSlKiDLZ3Z5AjBI3KyVTNs",
+    authDomain: "cryptoquick-47135.firebaseapp.com",
+    projectId: "cryptoquick-47135",
+    storageBucket: "cryptoquick-47135.appspot.com",
+    messagingSenderId: "574950436738",
+    appId: "1:574950436738:web:851f52a4b0a52a20f1e99b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const realtimeDB = getDatabase(app);
+export default app;
