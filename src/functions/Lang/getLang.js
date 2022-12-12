9a445352804294bf3eaf27649c43ef1398818376
integ <cergocergo41@gmail.com>
2022-12-12 11:02:41 +0300

    redact language set

--- src/functions/Lang/getLang.js
+++ src/functions/Lang/getLang.js
@@ -1,8 +1,23 @@
 export const getLang = () => {
     const lang = window?.localStorage.getItem('lang');
 
+    //ставим язык браузера
+    const langNew = () => {
+        const langInner = window.navigator.language;
+        if (langInner) {
+            if(langInner.includes("ru")){
+                return "rus";
+            }else {
+                return "eng";
+            }
+        }else {
+            return false;
+        }
+    }
+
+    //ставим язык
     if (!lang || (lang !== 'eng' && lang !== 'rus')) {
-        window?.localStorage.setItem('lang','eng');
+        window?.localStorage.setItem('lang',langNew()?langNew():'eng');
         return 'eng';
     }
 
